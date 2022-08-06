e39811ecf01e35f1282a87f78294a44f483beec2
Guido Mauricio Mantilla Tarazona <guido.mantilla@yahoo.com>
2022-08-06 16:23:53 -0500

    some refactor changes

--- tools/license.go
+++ tools/license.go
@@ -1,3 +1,3 @@
 package tools
 
-//go:generate go-license -format md -input ../../go.sum -output ../../INHERITED_LICENSES.md
+// //go:generate go-license -format md -input ../go.sum -output ../docs/INHERITED_LICENSES.md
--- tools/mockgen.go
+++ tools/mockgen.go
@@ -1,10 +1,10 @@
 package tools
 
-//go:generate mockgen -package datasource -destination ../tests/mocks/misc/datasource/MockDBDataSource.go 				jukebox-app/src/misc/datasource DBDataSource
+//go:generate mockgen -package datasource -destination ../tests/mocks/datasource/MockDBDataSource.go 				jukebox-app/pkg/datasource DBDataSource
 
-//go:generate mockgen -package environment -destination ../tests/mocks/misc/environment/MockEnvironment.go 				jukebox-app/src/misc/environment Environment
+//go:generate mockgen -package environment -destination ../tests/mocks/environment/MockEnvironment.go 				jukebox-app/pkg/environment Environment
 
-//go:generate mockgen -package properties -destination ../tests/mocks/misc/properties/MockPropertySource.go 			jukebox-app/src/misc/properties PropertySource
-//go:generate mockgen -package properties -destination ../tests/mocks/misc/properties/MockProperties.go 				jukebox-app/src/misc/properties Properties
+//go:generate mockgen -package properties -destination ../tests/mocks/properties/MockPropertySource.go 				jukebox-app/pkg/properties PropertySource
+//go:generate mockgen -package properties -destination ../tests/mocks/properties/MockProperties.go 					jukebox-app/pkg/properties Properties
 
-//go:generate mockgen -package transaction -destination ../tests/mocks/misc/transaction/MockDBTransactionHandler.go 	jukebox-app/src/misc/transaction DBTransactionHandler
+//go:generate mockgen -package transaction -destination ../tests/mocks/transaction/MockDBTransactionHandler.go 		jukebox-app/pkg/transaction DBTransactionHandler
