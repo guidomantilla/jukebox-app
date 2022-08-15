e1fba116b533752073e0471acabcc6f43f74dc2c
Guido Mauricio Mantilla Tarazona <guido.mantilla@yahoo.com>
2022-08-15 18:11:46 -0500

    some refactor changes

--- tools/mockgen.go
+++ tools/mockgen.go
@@ -1,10 +1,10 @@
 package tools
 
-//go:generate mockgen -package mocks -destination ../tests/mocks/MockDBDataSource.go 				jukebox-app/pkg/datasource DBDataSource
+//go:generate mockgen -package mocks -destination ../tests/mocks/MockRelationalDataSource.go 			jukebox-app/pkg/datasource RelationalDataSource
 
-//go:generate mockgen -package mocks -destination ../tests/mocks/MockEnvironment.go 				jukebox-app/pkg/environment Environment
+//go:generate mockgen -package mocks -destination ../tests/mocks/MockEnvironment.go 					jukebox-app/pkg/environment Environment
 
-//go:generate mockgen -package mocks -destination ../tests/mocks/MockPropertySource.go 				jukebox-app/pkg/properties PropertySource
-//go:generate mockgen -package mocks -destination ../tests/mocks/MockProperties.go 					jukebox-app/pkg/properties Properties
+//go:generate mockgen -package mocks -destination ../tests/mocks/MockPropertySource.go 					jukebox-app/pkg/properties PropertySource
+//go:generate mockgen -package mocks -destination ../tests/mocks/MockProperties.go 						jukebox-app/pkg/properties Properties
 
-//go:generate mockgen -package mocks -destination ../tests/mocks/MockDBTransactionHandler.go 		jukebox-app/pkg/transaction DBTransactionHandler
+//go:generate mockgen -package mocks -destination ../tests/mocks/MockRelationalTransactionHandler.go	jukebox-app/pkg/transaction RelationalTransactionHandler
